2d0c08b10418bee297234f45e7f793803f921cd6
Honey Amilineni <ahoneychoudary@gmail.com>
2025-12-15 19:04:46 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask, request, jsonify, render_template
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+# configuration
+# In production, we often use /tmp for temporary storage if we don't have a database
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB limit
+
+@app.route('/')
+def index():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    if file:
+        filename = secure_filename(file.filename)
+        save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(save_path)
+        return jsonify({'message': f'Successfully uploaded {filename}'}), 200
+
+if __name__ == '__main__':
+    # This is only used when running locally
+    app.run(debug=True)
